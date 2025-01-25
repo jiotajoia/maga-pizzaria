fea4e1b4abbac543dd19ae595f427447ddcd1ead
jiotajoia <bleibeducuz@gmail.com>
2025-01-25 16:04:16 -0300

    handlebars e atualização do readme

---
+++ readme.md
@@ -0,0 +1,10 @@
+Node.js
+Typescript
+
+#Comandos para instalar:
+
+npm install typeorm --save
+npm install reflect-metadata --save
+npm install @types/node --save-dev
+npm install pg --save
+npm install express-handlebars
--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,15 @@
 import express from 'express';
 import "reflect-metadata";
+import { engine } from 'express-handlebars';
+import path from 'path';
 import { router } from './routes/web';
 
 const app = express();
+
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+
+app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.json());
 app.use('/maga',router);
